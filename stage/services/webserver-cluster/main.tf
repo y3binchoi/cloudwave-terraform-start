30f09e5a26fef7fb0dfcf64dbe7c7c61711dc097
devin <dev.devin.kr@gmail.com>
2024-08-06 14:37:05 +0900

    stage: change instance type

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -6,7 +6,7 @@ module "webserver_cluster" {
  cluster_name = "webservers-stage"
  db_remote_state_bucket = "cloudwave-cj-st26"
  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
- instance_type = "t2.micro"
+ instance_type = "t3.micro"
  min_size = 2
  max_size = 4
 }
